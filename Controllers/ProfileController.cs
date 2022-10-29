340e6dfc69be354543bb0290cc38391c00d2b69d
mediat-100 <yusuftomiwa740@gmail.com>
2022-10-29 16:42:55 +0100

    Add project files.

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,24 @@
+﻿using hng_task_1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hng_task_1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProfileController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var user = new Profile()
+            {
+                slackUsername = "Tomz",
+                backend = true,
+                age = 24,
+                bio = "Backend Engineer"
+            };
+
+            return Ok(user);
+        }
+    }
+}
---
+++ Models/Profile.cs
@@ -0,0 +1,10 @@
+﻿namespace hng_task_1.Models
+{
+    public class Profile
+    {
+        public string? slackUsername { get; set; }
+        public bool backend { get; set; }
+        public int age { get; set; }
+        public string? bio { get; set; }
+    }
+}
